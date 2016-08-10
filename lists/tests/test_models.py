9a19c04a9ad8e61e2765cc9029e90058dbc065a2
subha-py <sbera@ixiacom.com>
2016-08-10 12:34:52 +0530

    Split out unit tests into two files

---
+++ lists/tests/test_models.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from lists.models import Item,List
+
+
+class ListAndItemModelsTest(TestCase):
+        def test_saving_and_retrieving_items(self):
+            list_=List.objects.create()
+
+            first_item=Item.objects.create(text='The first (ever) list item',list=list_)
+            second_item=Item.objects.create(text='Item the second',list=list_)
+
+            saved_list=List.objects.first()
+            self.assertEqual(saved_list,list_)
+
+            saved_items=Item.objects.all()
+            self.assertEqual(saved_items.count(),2)
+
+            first_item_saved=saved_items[0]
+            second_item_saved=saved_items[1]
+            self.assertEqual('The first (ever) list item',first_item_saved.text)
+            self.assertEqual('Item the second',second_item_saved.text)
+            self.assertEqual(first_item_saved.list,list_)
+            self.assertEqual(second_item_saved.list,list_)
+
