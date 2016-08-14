d4a1dea76a6e59e51a56f6979016718252f015b5
subha-py <sbera@ixiacom.com>
2016-08-14 18:16:44 +0530

    Added qunit.js

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -2,6 +2,9 @@ from unittest import skip
 from .base import FunctionalTest
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         #Edith goes to the home page and accidentally tries to submit
         #an empty list item. She hits enter on the empty input box
@@ -36,4 +39,17 @@ class ItemValidationTest(FunctionalTest):
         #she sees a helpful error message
         self.check_for_row_in_list_table('1: Buy wellies')
         error=self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text,'You have already got this in your list')
+        self.assertEqual(error.text,"You've already got this in your list")
+
+    def test_error_messages_are_cleared_on_input(self):
+        #Edith starts a new list in a way that causes a validation error:
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error=self.browser.find_element_by_css_selector('.has-error')
+        self.assertTrue(error.is_displayed())
+
+        #she starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+        #she is pleased to see that the error message disappears
+        error=self.get_error_element()
+        self.assertFalse(error.is_displayed())
