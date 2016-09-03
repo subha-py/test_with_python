91111964e7b75aa45dc351c412a6294f3ee87a14
subha-py <subha4exam@gmail.com>
2016-09-03 14:19:58 +0530

    added passed ft for mailing

--- superlists/settings.py
+++ superlists/settings.py
@@ -118,3 +118,21 @@ EMAIL_HOST_USER='subha.py@gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASSWORD')
 EMAIL_USE_TLS=True
+
+#logging settings
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
